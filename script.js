8d32ab72604ed990528dff44ddf04c57e8cab537
Lilith <asihab47@gmail.com>
2026-05-31 04:03:06 +0700

    Refactor member list rendering in script.js

--- script.js
+++ script.js
@@ -1,57 +1,64 @@
 const members = [
-
 {
-name:"Lilith",
-rank:"Guild Master",
-avatar:"https://i.pravatar.cc/150?img=1"
+name: "Lilith",
+rank: "Guild Master",
+avatar: "https://i.pravatar.cc/150?img=1"
 },
-
 {
-name:"Sizy",
-rank:"Officer",
-avatar:"https://i.pravatar.cc/150?img=2"
+name: "Sizy",
+rank: "Officer",
+avatar: "https://i.pravatar.cc/150?img=2"
 },
-
 {
-name:"Aori",
-rank:"Raid Leader",
-avatar:"https://i.pravatar.cc/150?img=3"
+name: "Aori",
+rank: "Raid Leader",
+avatar: "https://i.pravatar.cc/150?img=3"
 },
-
 {
-name:"Ristek",
-rank:"Elite Member",
-avatar:"https://i.pravatar.cc/150?img=4"
+name: "Ristek",
+rank: "Elite Member",
+avatar: "https://i.pravatar.cc/150?img=4"
 },
-
 {
-name:"Kuro",
-rank:"Member",
-avatar:"https://i.pravatar.cc/150?img=5"
+name: "Kuro",
+rank: "Member",
+avatar: "https://i.pravatar.cc/150?img=5"
 }
-
 ];
 
-document.getElementById("memberCount").innerText =
-members.length;
+const memberCount =
+document.getElementById("memberCount");
 
-const container =
+const memberList =
 document.getElementById("memberList");
 
-members.forEach(member=>{
+if (memberCount) {
+memberCount.textContent = members.length;
+}
 
-container.innerHTML += `
+if (memberList) {
 
+let html = "";
+
+members.forEach(member => {
+
+html += `
 <div class="member">
 
-<img src="${member.avatar}">
+<img
+src="${member.avatar}"
+alt="${member.name}"
+>
 
 <h3>${member.name}</h3>
 
 <span>${member.rank}</span>
 
 </div>
-
 `;
 
 });
+
+memberList.innerHTML = html;
+
+}
